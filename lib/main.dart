fef90132baf138d5a57a250462aded1afb89b0a5
quinnter <lauraquinn@Lauras-Air.broadband>
2019-05-28 19:34:16 +0100

    created container with a raisedButton widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,39 @@
 import "package:flutter/material.dart";
 
 void main() => runApp(MyApp());
-  //arg of runApp has to be a widget
-  //void this method returns nothing
-  //void is a type
-
+//arg of runApp has to be a widget
+//void this method returns nothing
+//void is a type
 
 class MyApp extends StatelessWidget {
   @override //annotation, this is a method that overrides another method
 
-  Widget build(BuildContext context) { //Widget + BuildContext is a class defined by flutter package
+  Widget build(BuildContext context) {
+    //Widget + BuildContext is a class defined by flutter package
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("EasyList"),
-        ),
-        body: Card(
-          child: Column( //whats inside the card
-            children: <Widget>[ 
-              Image.asset("assets/food.jpg"),
-              Text("Food Paradise")
-           ],
-          ), 
-        ),
-      ),
-    ); 
+          appBar: AppBar(
+            title: Text("EasyList"),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  child: Text("Add Product"),
+                  onPressed: () {},
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset("assets/food.jpg"),
+                    Text("Food Paradise")
+                  ],
+                ),
+              ),
+            ],
+          )),
+    );
   } //core root widget, needed in every app
 }

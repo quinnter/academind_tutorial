d916cf8f48103098bd4d2341fb1bd46cf4994e29
quinnter <lauraquinn@Lauras-Air.broadband>
2019-05-28 19:13:24 +0100

    used types in MyApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import "package:flutter/material.dart";
 
-main() {
+void main() => runApp(MyApp());
+  //arg of runApp has to be a widget
+  //void this method returns nothing
+  //void is a type
 
-}
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  @override //annotation, this is a method that overrides another method
+
+  Widget build(BuildContext context) { //Widget + BuildContext is a class defined by flutter package
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(
-        title: Text("EasyList"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("EasyList"),
         ),
       ),
     ); 
